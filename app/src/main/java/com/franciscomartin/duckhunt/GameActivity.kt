14d86b1eb2d2c6d6ef08f2cc976f12e74af5623f
Francisco <javiermartincuenca@gmail.com>
2019-04-20 15:57:11 +0200

    Nickname visualized on game screen

--- app/src/main/java/com/franciscomartin/duckhunt/GameActivity.kt
+++ app/src/main/java/com/franciscomartin/duckhunt/GameActivity.kt
@@ -2,11 +2,16 @@ package com.franciscomartin.duckhunt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.franciscomartin.duckhunt.commons.Constants
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val nick:String = intent.extras.getString(Constants.EXTRA_NICK)
+        textViewNick.text = nick
     }
 }
